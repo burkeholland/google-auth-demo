fd4c41916e0046966beb6acf14ef013f2161b174
Burke Holland <burkeholland@gmail.com>
2015-09-21 17:06:47 -0500

    I have no idea what i'm doing

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Everlive.Sdk.Core;
+
+namespace google_auth_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string SignIn(string token)
+        {
+            var app = new EverliveApp("5tnum1NuePyeHNwk");
+
+            try {
+                app.WorkWith().Authentication().LoginWithGoogle(token).ExecuteSync();
+                return "SUCCESS!";
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return $"Operation Failed: {ex.Message}"; 
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="google_auth_demo.MvcApplication" Language="C#" %>
